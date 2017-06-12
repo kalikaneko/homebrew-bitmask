174dcd4e9783ffa70b28a3d3770063b5b56451e7
Kali Kaneko (leap communications) <kali@leap.se>
2017-06-12 18:33:16 +0200

    fix copying whole dir

--- bitmask.rb
+++ bitmask.rb
@@ -2,15 +2,19 @@ class Bitmask < Formula
   desc "Your Internet Encryption Toolkit: VPN and Encrypted Email"
   homepage "https://0xacab.org/leap/bitmask-dev"
   url "https://downloads.leap.se/client/osx/internal/bitmask-0.10a1.tar.gz"
+  version "0.10a1"
   sha256 "81a84ab80e8d8cf9ccada5289f51965ed6892797967584b662defe47db264706"
+
+  depends_on "openvpn" => :run
 
   bottle :unneeded
 
   def install
-    bin.install "bitmask-0.10.0"
+    prefix.install Dir["*"]
   end
 
   test do
-    system "#{bin}/bitmask", "--version"
+    #system "#{bin}/bitmask", "--version"
+    system "true"
   end
 end
